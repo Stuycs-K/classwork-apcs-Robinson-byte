d1d558c2013733b45912c5a59c99559464a3d10c
Robinson Zhang <zrob66453@gmail.com>
2024-09-30 04:47:25 -0400

    I forgot the comma space if there are more than one number, added some tests cases, and fixed my public class (It didn't match with my .java file name)

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -1,14 +1,19 @@
-public class MyArrays{
+public class MyArray{
   public static String aryToString(int[]nums){
     String arrayConvert= "[";
-    for (int i = 0; i<= nums.length; i++){
+    for (int i = 0; i< nums.length; i++){
       arrayConvert+=nums[i];
+      if(i<nums.length-1){
+        arrayConvert+=", ";
+      }
     }
     arrayConvert+="]";
     return arrayConvert;
   }
 public static void main(String[] args){
-  System.out.println(aryToString(new int[] {1,2,3,4,5}));
-  System.out.println(aryToString(new int[] {}));
+  System.out.println("Test 1" + aryToString(new int[]{1,2,3,4,5}));
+  System.out.println("Test 2" + aryToString(new int[] {}));
+  System.out.println("Test 3" + aryToString(new int[] {321}));
+  System.out.println("Test 4" + aryToString(new int[] {3,3,3,3}));
 }
 }
