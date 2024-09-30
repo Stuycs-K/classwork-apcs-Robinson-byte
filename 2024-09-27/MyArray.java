87d2357e2c5c2f1680b37d1e72336857322cc472
Robinson Zhang <zrob66453@gmail.com>
2024-09-30 05:34:26 -0400

    wrote the concatArray and wrote two testers for the code

--- 2024-09-27/MyArray.java
+++ 2024-09-27/MyArray.java
@@ -10,6 +10,7 @@ public class MyArray{
     arrayConvert+="]";
     return arrayConvert;
   }
+
   public static int[] returnCopy(int[] ary){
     int[] result = new int[ary.length];
     for(int i = 0; i< ary.length; i++){
@@ -17,12 +18,27 @@ public class MyArray{
     }
     return result;
   }
+
+  public static int[] concatArray(int[] ary1, int[] ary2){
+    int[] result = new int[ary1.length+ary2.length];
+    for (int i =0; i<ary1.length;i++){
+      result[i]=ary1[i];
+      // a copy of the numbers in ary1 are in result
+    }
+    for (int i =0; i<ary2.length;i++){
+    result[ary1.length+i]=ary2[i];
+    //numbers in ary2 are copied into result after the numbers in ary1
+    }
+    return result;
+  }
+
 public static void main(String[] args){
   //Testing aryToString
   System.out.println("Test 1" + aryToString(new int[]{1,2,3,4,5})); //ary with multiple numbers
   System.out.println("Test 2" + aryToString(new int[] {})); //empty ary
   System.out.println("Test 3" + aryToString(new int[] {321})); //one number ary
   System.out.println("Test 4" + aryToString(new int[] {3,3,3,3})); //ary with multiple of the same number
+
   //Testing returnCopy
   int [] testers=  {1,2,3,4,5,6,7}; //ary with multiple numbers
   int [] mine =returnCopy(testers); //run returnCopy here then compare using aryToString
@@ -38,5 +54,16 @@ public static void main(String[] args){
   System.out.println("Test 3: Original array" + aryToString(testersEmpty));
   System.out.println("Test 3: Copied array" + aryToString(mineEmpty));
 
+  //Testing concatArry
+  //two arrays with numbers in them
+  int [] ary1 = {12,3,321,32};
+  int [] ary2 = {154,1232,9231};
+  int [] myConcat = concatArray(ary1,ary2);
+  System.out.println("Concatenated array" + aryToString(myConcat));
+
+  //one empty array
+  int [] aryEmpty = {};
+  int [] myConcatEmpty = concatArray(aryEmpty,ary2);
+  System.out.println("Concatenated array" + aryToString(myConcatEmpty));
 }
 }
