5d864c85489e43fba16178394a1ae827c40644b8
Robinson Zhang <zrob66453@gmail.com>
2024-09-30 23:16:44 -0400

    I wrote the swapRC code and a test case for it

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -48,12 +48,28 @@ public class ArrayMethods {
     return sum;
   }
 
+  public static int [][] swapRC(int[][]nums){
+    int columns = nums[0].length;
+    int rows = nums.length;
+    int[][] swapped = new int[columns][rows];
+    for (int i = 0; i<rows; i++){
+      for (int j=0; j<columns; j++){
+        swapped [j][i]= nums [i][j];
+      }
+    }
+    return swapped;
+  }
+
   public static void main(String[] args) {
     // array 2D sum test cases
-    // System.out.println(arr2DSum(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}) == 45);
-    // System.out.println(arr2DSum(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}) == 45);
+    System.out.println(arr2DSum(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}) == 45);
+    System.out.println(arr2DSum(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}) == 45);
 
     // aryToString for 2d tests
     System.out.println(aryToString(new int[][] {{1, 2}, {4, 5}, {6, 7}}));
+
+    //swapRC tests
+    int[][] result = swapRC(new int[][]{{1, 2, 3},{4, 5, 6}});
+    System.out.println(aryToString(result));
   }
 }
