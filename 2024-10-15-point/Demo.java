4f88ca07f48257272bec9ae76143028f9a892c85
Robinson-byte <robinsonz@nycstudents.net>
2024-10-15 22:49:27 -0400

    wrote down the distance method in Demo.java

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -1,11 +1,6 @@
 public class Demo{
-
-  /*
-  Write this method third. Test it then move on
-  to the others.
-  */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(Math.pow(b.getX()-a.getX(),2)+Math.pow(b.getY()-a.getY(),2));
   }
 
   public static void main(String[]args){
