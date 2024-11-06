0939937d59feaaeb9cab43b1c6613d1b54f8fd56
Robinson-byte <robinsonz@nycstudents.net>
2024-11-05 19:14:42 -0500

    I copied the finish code from class to my files

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -1,36 +1,43 @@
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+
 
 class Main {
+  Scanner sc = new Scanner("Some String (or File) Here");
     public static void main(String[] args) {
       String text = "This is a sample\nOf how to read\ntext!";
-      Scanner input = new Scanner(text);
+
       // creates a Scanner object to read throug the String
       Scanner input = new Scanner(text);
+
+      //you don't want a for loop here unless you know the size of the data set
+
+      while(input.hasNext()){
+        System.out.println(input.next());
+      }
+
+      Scanner sc1 = new Scanner("This is a bunch of words");
+      while(sc1.hasNext()){
+        System.out.println(sc1.next());
+      }
+     
+      Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+      double sum = 0;
+      while(sc2.hasNext()){
+        sum = sum + (sc2.nextDouble());
+        System.out.println(sum);
+      }
+      System.out.println(sum);
+
+      /*
       while(input.hasNextLine()){
         System.out.println(input.nextLine());
+        //do something with the next line of the file
       }
-      //you don't want a for loop here unless you know the size of the data set
-    }
-    input.close();
-    
-  }
-
-public class ReadFile {
-
-  public static void main(String[] args) {
-    //2 Opening a file requires a try/catch
-    try {
-      File file = new File("PUT_FILENAME_HERE");//1
-      Scanner input = new Scanner(file);
-      //CODE THAT SCANS THE FILE.
-      input.close();//releases the file from your program
-
-    } catch (FileNotFoundException ex) {
-      //File not found what should you do?
-      System.out.println("File not found");
-      return; //you can return from a void function just don't put a value.
+      int sum = 0;
+      while(input.hasNextInt()){
+        sum+=input.nextInt();//read all the numbers and add to sum
+      } */
     }
   }
-}
---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+//Q how many triangles are valid?
+public class TriangleTester{
+   
+    public static void main(String[] args) {
+        countTrianglesA("inputTri.txt");
+        countTrianglesB("inputTri.txt");
+    }
+
+    public static boolean validTriangle(int a, int b, int c){
+        if(a + b > c &&
+           b + c > a &&
+           c + a > b) return true;
+        return false;
+    }
+
+    //Part A
+    public static int countTrianglesA(String filename){
+        int sum = 0;
+        try{
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while(input.hasNextLine()){
+                if(validTriangle(Integer.parseInt(input.next()),
+                                 Integer.parseInt(input.next()),
+                                 Integer.parseInt(input.next()))) sum = sum + 1;
+                if(input.hasNextLine()) input.nextLine();
+            }
+            input.close();
+        }catch(FileNotFoundException ex){
+            System.out.println("File not found");
+        }
+
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static int countTrianglesB(String filename){
+        int sum = 0;
+        try{
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+
+            while(input.hasNextLine()){
+                int side101 = Integer.parseInt(input.next());
+                int side201 = Integer.parseInt(input.next());
+                int side301 = Integer.parseInt(input.next());
+
+                int side102 = Integer.parseInt(input.next());
+                int side202 = Integer.parseInt(input.next());
+                int side302 = Integer.parseInt(input.next());
+
+                int side103 = Integer.parseInt(input.next());
+                int side203 = Integer.parseInt(input.next());
+                int side303 = Integer.parseInt(input.next());
+
+                if(validTriangle(side101, side102, side103)) sum = sum + 1;
+                if(validTriangle(side201, side202, side203)) sum = sum + 1;
+                if(validTriangle(side301, side302, side303)) sum = sum + 1;
+                if(input.hasNextLine()) input.nextLine();
+            }
+            input.close();
+        }catch(FileNotFoundException ex){
+            System.out.println("File not found");
+        }
+
+        System.out.println(sum);
+        return sum;
+    }
+
+   
+}
