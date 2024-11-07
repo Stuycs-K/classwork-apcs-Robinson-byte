2fe6f8c453b1ce306278d3aff34f2ee44977756f
Robinson Zhang <robinsonz@nycstudents.net>
2024-11-07 10:39:37 -0500

    finished first two steps, switched

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -4,6 +4,9 @@ public class Animal {
   private String name;
 
   public Animal(String noise, int age, String name){
+    this.noise = noise;
+    this.age = age;
+    this.name = name;
     //initialize the values (hint use this.varname to clarify)
   }
 
@@ -17,6 +20,9 @@ public class Animal {
   Note: you MUST use getName() getNoise() here! You will see why later.
   */
   public void speak(){
+    System.out.println("My name is " + getName() + ".");
+    System.out.println("I am " + age + " years old.");
+    System.out.println("I say " + getNoise() + ".");
 
   }
 
