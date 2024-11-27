99e36a340ba2894e8e3c2228f061c0365e6e21d4
Robinson-byte <robinsonz@nycstudents.net>
2024-11-27 02:36:42 -0500

    tweaked my code to have it recongize if a place has been visited twice for part 2

--- 2024-11-18-advent2016/Advent 1/Adventday1.java
+++ 2024-11-18-advent2016/Advent 1/Adventday1.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Adventday1{
   public static void main(String[] args){
@@ -21,6 +23,8 @@ public class Adventday1{
     int x =0;
     int y = 0;
     int directions = 0; //0 N; 1 E; 2 S; 3 W;
+    List<int[]> visited = new ArrayList<>();
+    visited.add(new int[]{0,0});
 
     for(String step: steps){
       char turn = step.charAt(0);
@@ -32,13 +36,20 @@ public class Adventday1{
       else if (turn == 'L'){
         directions = (directions + 3)%4;
       }
-      switch(directions){
-        case 0: y+= distance; break;
-        case 1: x+= distance; break;
-        case 2: y-= distance; break;
-        case 3: x-= distance; break;
+      for (int i = 0; i < distance; i++) {
+        if (directions == 0) y++;
+        else if (directions == 1) x++;
+        else if (directions == 2) y--;
+        else if (directions == 3) x--;
+
+        for (int [] pos: visited){
+          if (pos[0]== x && pos[1]==y){
+            return Math.abs(x)+Math.abs(y);
+        }
+        }
+        visited.add(new int []{x,y});
       }
     }
-    return Math.abs(x)+Math.abs(y);
+    return -1;
   }
 }
