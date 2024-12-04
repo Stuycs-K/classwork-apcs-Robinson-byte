92521c746b39cd063a1af1ac14bb3098e3365f84
Robinson-byte <robinsonz@nycstudents.net>
2024-12-04 06:17:55 -0500

    the animation works but I don't know if the background colors work

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -10,28 +10,34 @@ public class ColorDemo{
     }
   }
 
-  public static void main(String[] args){
-    String[] frames = {
-      " O | /|\\ / \\",
-      " O | /|\\ _| ",
-      " O | /|\\  |\\",
-      " O | /|\\ _|_"
+  private static void printStickFigure(String between){
+    String walking = "  O  \n  |  \n /|\\ \n/   \\";
+    String standing = "  O  \n  |  \n _|_ \n | |";
+    String[] walkingParts = walking.split("\n");
+    String[] standingParts = standing.split("\n");
 
-    };
-    
+    for (int i = 0; i<walkingParts.length;i++){
+      System.out.println(walkingParts[i]+ between + standingParts[i]);
+    }
+  }
+
+  public static void main(String[] args){    
     String[] themes = {
       "\u001b[38;2;255;87;51m",
       "\u001b[38;2;255;223;0m",
       "\u001b[38;2;135;206;235m",
       "\u001b[38;2;25;25;112m"
     };
-    
-    System.out.print(HIDE_CURSOR);
 
+    System.out.print(HIDE_CURSOR);
     for (int i = 0; i<50; i++){
       System.out.print(CLEAR_SCREEN);
       System.out.print(themes[i%themes.length]);
-      System.out.println(frames[i%frames.length]);
+
+      int walkingSpace = (i%10)+1;
+      String space = " ".repeat(walkingSpace);
+
+      printStickFigure(space);
       System.out.print("\u001b[0m");
 
       sleep(200);
