bbc5918d3ddafe31cf624334e7cd51d84cb3115b
Robinson-byte <robinsonz@nycstudents.net>
2024-12-04 05:30:39 -0500

    Restarting my code

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -4,13 +4,6 @@ public Class ColorDemo{
   public static final String SHOW_CURSOR =  "\u001b[?25h";
 
   public static void main(Strings[] args){
-    int personColor;
-    for(int i =0;i<60;i++){
-      System.out.print("\u001b[48;2;135;206;235m"); //background skyblue
-      System.out.print("\u001b["+personColor); // person is white
 
-// 0
-// |
-    }
   }
 }
