03b92e4912675aee83eb7999f05be928133af309
Robinson-byte <robinsonz@nycstudents.net>
2024-12-05 05:44:16 -0500

    started coding the screen, wrote drawScreen

---
+++ 2024-12-04-ColorScreen/Driver.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class Driver{
+    System.out.print(Text.CLEAR_SCREEN);
+    System.out.print(Text.HIDE_CURSOR);
+
+    drawScreen();
+
+    int[] randomNumbers = generateRandom(3);
+
+    displayRandomNumers(randomNumbers);
+
+    separate();
+
+    drawMiddle();
+
+    Text.go(31,0);
+    System.out.println(Text.RESET);
+}
+
+public static void drawScreen(){
+    for(int i = 1; i <=80;i++){
+        Text.go(1,i);
+        Text.color(Text.BRIGHT, Text.MAGENTA, Text.background(Text.YELLOW));
+        System.out.print("◼");
+
+        Text.go(30, i);
+        System.out.print("◼");
+    }
+    for(int i = 2; i <=30;i++){
+        Text.go(i,1);
+        Text.color(Text.BRIGHT, Text.MAGENTA, Text.background(Text.YELLOW));
+        System.out.print("◼");
+
+        Text.go(i,80);
+        System.out.print("◼");
+    }
+    Text.go(1,1);
+    System.out.print("◼");
+    Text.go(1,80);
+    System.out.print("◼");
+    Text.go(30,1);
+    System.out.print("◼");
+    Text.go(30,80);
+    System.out.print("◼");
+}
