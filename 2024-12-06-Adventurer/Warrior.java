1cec5b442a07ba2788bf9cafd24fc5269dff8343
Robinson-byte <robinsonz@nycstudents.net>
2024-12-06 10:58:02 -0500

    Starting writing a subclass for Warriors that use rage as thier special attack

---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+
+public class Warrior extends Adventurer{
+    public int rage; 
+    private final int maxRage; 
+
+    public Warrior(String name, int hp){
+        super(name,hp);
+        this.rage=0;
+        this.maxRage=10;
+    }
+
+    @Override
+    public int getSpecial(){
+        return this.rage;
+    }
+
+    @Override
+    public void setSpecial(int n){
+        if(n>maxRage){
+            this.rage = maxRage;
+        }
+        else if (n<0){
+            this.rage =0;
+        }
+        else{
+            this.rage=n;
+        }
+    }
+
+    @Override
+    public int getSpecialMax(){
+        return this.maxRage;
+    }
+
+    @Override
+    public String attack(Adventurer other){
+        Random rand = new Random();
+        int damage = rand.nextln(10)+1;
+        other.applyDamage(damage);
+        return this.getName() + " attacks " + other.getName() + " for " + damage + " damage ";
+    }
+
+    @Override
+    public String support(Adventurer other){
+        Random rand = new Random();
+        int heal = rand.nextln(3)+1;
+        other.setHP(Math.min(other.getHP()+heal,other.getmaxHP));
+        return this.getName() + " supports " + other.getName() + " healing for " + heal + " HP ";
+    }
+
+    @Override
+    public String support(){
+        Random rand = new Random();
+        int heal = rand.nextln(3)+1;
+        this.setHP(Math.min(this.getHP()+heal,this.getmaxHP));
+        return this.getName() " heals themselves for " + heal + " HP ";
+    }
+
+    @Override
+    public String specialAttack(Adventurer other){
+        if(this.getSpecial()< 3){
+            return this.getName() + " tried to use their special, but doesn't have enough Rage";
+        }
+        else{
+            this.setSpecial(this.getSpecial()-3);
+            int damage = 15;
+            other.applyDamage(damage);
+            return this.getName()+ " used a special attack on " + other.getName() + " for 15 damage";
+        }
+    }
+}
