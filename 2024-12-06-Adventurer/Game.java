b8693fa57a1ac31a0c5929d366f74755db26acb9
Robinson-byte <robinsonz@nycstudents.net>
2024-12-09 11:01:13 -0500

    started the code mostly wrote print methods and initialized the player and enemy adentuer

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,6 +4,7 @@ class Game {
     //do this once
     Scanner userInput = new Scanner(System.in);
 
+    System.out.println("Welcome to my rpg! ");
     //You can do the rest many times:
 
     System.out.println("Enter username");
@@ -11,5 +12,16 @@ class Game {
     String userName = userInput.nextLine();
     //Do something with the input
     System.out.println("Username is: " + userName);
+
+    Adventuer player = new Warrior(playerName, 20);
+    Adventuer enemy = new codeWarrior("Conan",20);
+
+    System.our.println("Let the battle begin!! :)");
+
+    while(player.getHP()>0 && enemy.getHP()>0){
+      System.out.println("Player's health: " + player.getHP);
+      System.out.println("Enemy's health: " + enemy.getHP);
+      
+    }
   }
 }
