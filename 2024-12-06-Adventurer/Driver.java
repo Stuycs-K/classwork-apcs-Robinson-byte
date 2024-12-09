27bf452c81edfb273cd0448a1f69d14b54277295
Robinson-byte <robinsonz@nycstudents.net>
2024-12-08 23:54:35 -0500

    finished writing some test cases for the methods, seems to be working

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,9 +1,21 @@
 public class Driver{
-    static void main(String[] args){
+    public static void main(String[] args){
         Adventurer loki = new Warrior("Loki", 40);
         Adventurer thor = new Warrior("Thor", 80);
 
-        System.out.println(loki.attacks(thor));
-        System.out.println("Thor's health should be <80" + thor.getHP());
+        System.out.println(loki.attack(thor));
+        System.out.println("Thor's damaged health is "+ thor.getHP());
+
+        System.out.println(loki.support(thor));
+        System.out.println("Thor's healed health is "+ thor.getHP());
+
+        System.out.println(thor.support());
+        System.out.println("Thor's self healed health is "+ thor.getHP());
+
+        System.out.println(loki.specialAttack(thor));
+
+        loki.setSpecial(10);
+        System.out.println(loki.specialAttack(thor));
+        System.out.println("Thor's health after special attack is "+ thor.getHP());
     }
 }
